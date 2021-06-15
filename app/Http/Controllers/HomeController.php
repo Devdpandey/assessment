19cc9249f068397d2e9c1baf85d8ff948746f0fa
devendra2022 <devendra.pandeya+2022@olivegroup.io>
2021-06-15 14:29:21 +0545

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,19 +24,47 @@ class HomeController extends Controller
         $exists = Storage::disk('local')->exists('/exported-clients/clients-'.date('d-m-Y').'.csv');
         if($exists){
             $data = Storage::get('/exported-clients/clients-'.date('d-m-Y').'.csv');
-           
-            $csvData = array();
- 
-
-        while (($row = fgetcsv($data, 0, ",")) !== FALSE) {
-            $csvData[] = $row;
-        }
-         
-        dd(json_encode($csvData));
+            $clients = explode("\n", $data) ; // create an array of 2 entry (first line, and the rest).
+            // dd($clients);
+            $columns = $this->getColumnNames($clients);
+            $formData = $this->getColumnData($clients,$columns);
+            dd($formData);
         }
        return view('client.index',compact('clients'));
 
             	
+    }
+
+    private function getColumnNames($firstArray=null){
+        if(!$firstArray) return null;
+        $column = array();
+        $data = explode(",",$firstArray[0]);
+      
+        foreach($data as $key => $heading){
+            $column[] = $heading;
+        }
+        return $column;
+    }
+    private function getColumnData($clients=null,$columns=null){
+       
+        $responseData = array();
+        $i=0;
+        foreach($clients as $k=>$clientData){
+       
+            if($clientData == $clients[0]){
+                continue;
+            }else{
+                $response = array();
+                $data = explode(",",$clientData);
+                $k=0;
+                foreach($columns as $key=>$column){
+                    $response[] = array($column => @$data[$key]);
+                }
+            }
+            $i++;
+            $responseData[] = $response;
+        }
+        return $responseData;
     }
 
     public function create()
